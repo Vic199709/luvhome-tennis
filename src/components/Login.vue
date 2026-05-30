2f6415e31455425bf7009fbd6b7b09dee27a6881
ziyang <ziyang.c@btigroup.io>
2026-05-30 18:09:49 +0800

    appicon

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -73,7 +73,7 @@ const goToRegister = (e) => {
 
 <template>
   <div class="login-container">
-    <div class="login-logo">🎾</div>
+    <img src="/icon-192.png" class="login-logo" alt="網球聯盟" />
     <h1 class="login-title">網球聯盟會員登入</h1>
     <p class="login-subtitle">請輸入您的手機號碼以驗證會員身份</p>
     
